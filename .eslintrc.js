1e852e29ba9c7e258fee5af97a93c0f2577d0b3d
冯诚 <fengcheng@mgtv.com>
2022-08-23 19:32:03 +0800

    模板初始化

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['ktsn-vue'],
+
+  rules: {
+    'no-duplicate-imports': 'off',
+    'no-unused-expressions': 'off',
+    'no-empty': 'off',
+    'no-control-regex': 'off',
+    'vue/valid-template-root': 'off',
+    'vue/valid-v-model': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-v-html': 'off',
+    'vue/no-template-key': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+}
